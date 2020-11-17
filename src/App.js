ea1f97d9d81be8bc5372ae7d42aee53aaf0cf662
Ryan Viegas <rjkviegas@gmail.com>
2020-11-17 21:09:59 +0000

    Https

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   const onSubmit = (formData) => {
     const jsonObj = JSON.parse(formData.receipt)
-    fetch('http://3.8.131.158/makereceipt', {
+    fetch('https://api.ryansreceipts.com/makereceipt', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(jsonObj)
