2f45a32856b84c41ffe79c33259b10851c7d3a16
Ryan Viegas <rjkviegas@gmail.com>
2020-11-17 20:10:38 +0000

    MVP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {useForm} from 'react-hook-form';
+import Receipt from './components/Receipt';
 import './App.css';
 
 function App() {
+  const {register, handleSubmit} = useForm();
+  const [data, setData] = useState()
+
+  const onSubmit = (formData) => {
+    const jsonObj = JSON.parse(formData.receipt)
+    fetch('http://3.8.131.158/makereceipt', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(jsonObj)
+    })
+      .then(response => response.json())
+      .then(setData)
+      .catch(console.error)
+  }
+
+  if (data) {
+    // console.log(data.receipt)
+    return (
+      <div className="App">
+        <Receipt receipt={data.receipt} />
+      </div>
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <img src="ryansreceipts.PNG" alt="logo" />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <textarea placeholder="Menu and Order JSON" name="receipt" ref={register} rows="25" cols="100"/>
+      <br />
+      <button>Submit</button>
+    </form>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Receipt.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+export default function Receipt({ receipt}) {
+
+    function phoneNumFormat (aPhoneNum) {
+        return `+${aPhoneNum[0]} ` +
+        `(${aPhoneNum.slice(1, 4)}) ` +
+        `${aPhoneNum.slice(4, 7)}` +
+        `-${aPhoneNum.slice(7)}`
+    }
+
+    function usd(anAmount) {
+        return `$${Number.parseFloat(anAmount).toFixed(2)}`;
+    }
+
+    return (
+        <section id={receipt.shopName.toLowerCase().replace(/ /g, "-")}>
+            <h1>{receipt.shopName}</h1>
+            <h5>{receipt.address}</h5>
+            <h5>{phoneNumFormat(receipt.phone)}</h5>
+            <h3>Customer: {receipt.customer}</h3>
+            <h3>Items Ordered</h3>
+            <table id="table" class="center">
+                <thead>
+                    <tr>
+                        <th>Item</th>
+                        <th>Quantity</th>
+                        <th>Unit Price</th>
+                        <th>Discount %</th>
+                        <th>Discount Amount</th>
+                        <th>Total Amount</th>
+                    </tr> 
+                </thead>
+                <tbody>
+                    {receipt.items.map((item, i) => {
+                        return (
+                            <tr key={i}>
+                                <td>{item.id}</td>
+                                <td>{item.quantity}</td>
+                                <td>{usd(item.unitPrice)}</td>
+                                <td>{item.discPercent}</td>
+                                <td>{usd(item.discAmount)}</td>
+                                <td>{usd(item.totalAmount)}</td>
+                            </tr>
+                        )
+                    })}
+                    <tr>
+                        <th className="total" colSpan="5">Pre Tax Total :</th>
+                        <td>{usd(receipt.preTaxTotal)}</td>
+                    </tr>
+                    <tr>
+                        <th className="total" colSpan="5">Tax ({receipt.taxRate}%) :</th>
+                        <td>{usd(receipt.taxTotal)}</td>
+                    </tr>
+                    {receipt.totalDiscount &&
+                    <tr>
+                        <th id="total-discount" colSpan="6">
+                            {receipt.totalDiscount.percent}% off of orders over {usd(receipt.totalDiscount.limit)}
+                        </th>
+                    </tr>}
+                    <tr>
+                        <th className="total" id="final-total" colSpan="5">Final Amount :</th>
+                        <th>{usd(receipt.finalAmount)}</th>
+                    </tr>
+                    <tr>
+                        <th className="total" id="cash" colSpan="5">Cash :</th>
+                        <th>{usd(receipt.cash)}</th>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th className="total" id="change" colSpan="5">Change :</th>
+                        <th>{usd(receipt.change)}</th>
+                    </tr>   
+                </tfoot>
+            </table>
+        </section>
+    )
+}
